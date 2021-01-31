db838394a546b6691c7c53d6a9ffabadf99f38fe
Justin Bird <justin@thewordisbird.dev>
2021-01-31 11:36:55 -0800

    Broadcasting player status state.

--- index.js
+++ index.js
@@ -7,17 +7,70 @@ const io = require('socket.io')(server, {
   }
 });
 
+const PLAYER = {
+  id: '',
+  alias: '',
+  avitar: '',
+  progress: 0,// The character of the string the player is it
+  pace: 0,
+}
+
+let PLAYERS = []
+
 io.on('connection', socket => {
   // Initialize player object and broadcast to all players
-  console.log(`User ${socket.id} joined the game`)
-  io.emit('new player', {
-    id: socket.id,
-    alias: 'player' + socket.id,
-    avitar: '',
-    progress: 0,// The character of the string the player is it
-    pace: 0,
+  console.log(`User ${socket.id} joined the game. Client ${{...socket.Client}}`)
+  
+  addNewPlayer(socket);
+  
+  socket.on('disconnect', () => {
+    console.log(`Disconnecting user ${socket.id}`)
+    PLAYERS = PLAYERS.filter(player => {
+      return player.id !== socket.id;
+    })
+  });
+
+  socket.on('updatePlayer', updatedPlayer => {
+    // console.log('Player from client', updatedPlayer)
+    const playerIndex = PLAYERS.findIndex(player => {
+      // console.log(player.id, updatedPlayer)
+      return player.id === updatedPlayer.id
+    })
+    console.log('Player Index:', playerIndex)
+    const updatedPlayers = [...PLAYERS]
+    updatedPlayers[playerIndex] = {...updatedPlayer}
+    // console.log(updatedPlayers)
+    PLAYERS = updatedPlayers
+    // console.log(PLAYERS, playerIndex)
+    console.log('emitting updated players:', PLAYERS)
+    socket.emit('updatedPlayers', PLAYERS)
   })
+
+  
+  
+
+
+
 })
+
+const addNewPlayer = (socket) => {
+  console.log('in add player')
+  const player = {
+    ...PLAYER,
+    id: socket.id
+  };
+
+  PLAYERS = [
+    ...PLAYERS,
+    player
+  ];
+  
+  const response = {
+    playerId: player.id,
+    players: PLAYERS
+  };
+  socket.emit("NewPlayer", response);
+};
 
 server.listen(3001, () => {
   console.log('Listening on port 3001!');
